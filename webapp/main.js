3c133a74c259534efbac2455513fa53297847315
Oliver Arafat <arafato@amazon.de>
2015-07-08 11:50:07 +0200

    rest logic for api/slides completed

--- webapp/main.js
+++ webapp/main.js
@@ -25,7 +25,50 @@ app.get('/', function (req, res) {
  * REST INTERFACE
  */
 
-/* If no time(-range) parameter is given, all stored features ever released (since 2012) are returned.
+/* 
+ * Query parameters:
+ * 
+ * cat (optional) - compute | storagecdn | database | networking | admin-sec | deployment |
+ *       analytics | appservice | mobile | enterprise | other
+ *
+ * startdate (optional) - YYYY-MM-DD
+ * if without dateend, all matching items until current date are returned
+ * 
+ * enddate (optional) - YYYY-MM-DD
+ * if without datestart, all matching items until earliest date are returned
+*/
+app.get('/api/slides', function(req, res) {
+
+    var connection = mysql.createConnection({
+	host     : config.dbhost,
+	user     : config.dbuser,
+	password : config.dbpwd,
+	database : config.database,
+	port: 3306
+    });
+
+    connection.connect();
+    
+    queryUtils.getFeatures(connection, req.query, function(json) {
+	json = JSON.stringify(json);
+	var result = JSON.parse(json);
+	var features = [];
+	for(var i = 0; i < result.length; ++i) {
+	    
+	    if (features[result[i].category] === undefined) {
+		features[result[i].category] = [];
+	    }
+	    
+	    features[result[i].category].push(result[i]);
+	}
+
+	// res.render('revealjs-slides');
+    });
+
+    connection.end();
+});
+
+/* If no time(-range) parameter is given, all stored features ever released are returned.
  * 
  * Query parameters:
  * 
@@ -38,7 +81,7 @@ app.get('/', function (req, res) {
  * enddate (optional) - YYYY-MM-DD
  * if without datestart, all matching items until earliest date are returned
 */
-app.get('/api/feature', function(req, res, next) {
+app.get('/api/feature', function(req, res) {
 
     var connection = mysql.createConnection({
 	host     : config.dbhost,
